f854b3d7cca40ab8766d781549ae2e71de099528
HasanBaykal <baykalhh@gmail.com>
2019-12-04 14:24:06 +0100

    Front-End Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+      
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -16,73 +17,90 @@
             html, body {
                 font-family: 'Raleway', sans-serif;
                 height: 100%;
-                background: url("/images/garbagebackground2.jpg") no-repeat center center fixed;
+                background: url("/images/homepageklasbak.png") no-repeat center center fixed;
                 -webkit-background-size: cover;
                 -moz-background-size: cover;
                 background-size: cover;
                 -o-background-size: cover;
+                overflow-x: hidden;
+                -webkit-text-size-adjust: 100%;
+            } 
+
+            .active {
+              display: block;
             }
 
-                /* Container holding the image and the text */
-                .container-title {
-                  position: relative;
-                }
+            .menu {
+    position: fixed; 
+    width: 220px;
+    z-index: 1; 
+    top: 35px;
+}
 
-                /* Bottom right text */
-                .text-block-title {
-                position: absolute;
-                top: 9vw;
-                left: 43vw;
-                font-size: 2.5vw;
-                background: rgba(255, 255, 255, 0.0);
-                color: black;
-                transform: rotate(1.5deg);
-                }
+#menu-list {
+    background-color: rgba(120, 180, 0, 0.8);
+    font-size: 16px; 
+    padding: 0;
+}
+
+#menu-list > li {
+    display: block;
+}
+
+
+#menu-list > li > a {
+    font-family: 'Raleway', sans-serif; 
+    padding: 10px 0 10px 30px; 
+    text-decoration: none; 
+    display: block;
+    color: #fff; 
+}
+
+#menu-list > .active > a,
+#menu-list > li > a:hover { 
+    background-color: rgba(255,255,255,0.25);
+}
+
+.logo-klasbak {
+  margin-left: -4%;
+  margin-top: 22%;
+}
 
         </style>
     </head>
     <body>
 
-    <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
+    <img src="/images/klasbaklogonobackground.png" class="logo-klasbak">
+
+    <div class="menu visible-md visible-lg">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="http://127.0.0.1:8000">De Klasbak</a>
-    </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="http://127.0.0.1:8000/idee">Het Idee</a></li>
-        <li><a href="http://127.0.0.1:8000/top10">De Top 10</a></li>
-        <li><a href="http://127.0.0.1:8000/scholen">De Scholen</a></li>
-        <li><a href="http://127.0.0.1:8000/bestellen">Bestellen</a></li>
-        <li><a href="http://127.0.0.1:8000/contact">Contact</a></li>
+      <ul id="menu-list">
+        <li class="active"><a href="http://127.0.0.1:8000">Home</a></li>
+        <li class=""><a href="http://127.0.0.1:8000/idee">Het Idee</a></li>
+        <li class=""><a href="http://127.0.0.1:8000/top10">De Top 10</a></li>
+        <li class=""><a href="http://127.0.0.1:8000/scholen">De Scholen</a></li>
+        <li class=""><a href="http://127.0.0.1:8000/bestellen">Bestellen</a></li>
+        <li class=""><a href="http://127.0.0.1:8000/contact">Contact</a></li>
       </ul>
       @if (Route::has('login'))
-      <ul class="nav navbar-nav navbar-right">
+      <ul id="menu-list">
       @auth
       <li><a href="{{ url('/home') }}">Home</a></li>
       @else
-        <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span> Register</a></li>
+        <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span> Login</a></li>
         @if (Route::has('register'))
-        <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="{{ route('register') }}"><span class="glyphicon glyphicon-log-in"></span> Register</a></li>
         @endif
       @endauth
       </ul>
       @endif
     </div>
-  </div>
-</nav>
-
-<div class="container-title">
-            <div class="text-block-title">
-                <p>De Klasbak</p>
-                <p>Scheid Afval Met Plezier</p>
-            </div>
-        </div>
+    </div>
               
 </body>
 </html>
